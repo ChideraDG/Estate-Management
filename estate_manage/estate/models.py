a17990d1585d82bea59a75410c8b0433047a7104
Chidera Divine-gift <chrischidera6@gmail.com>
2024-07-04 14:15:36 +0100

    update

--- estate_manage/estate/models.py
+++ estate_manage/estate/models.py
@@ -1,5 +1,6 @@
 from django.core.validators import RegexValidator
 from django.db import models
+from django.utils import timezone
 
 
 class Registration(models.Model):
@@ -11,12 +12,17 @@ class Registration(models.Model):
     full_name = models.CharField(max_length=100, blank=False, null=False)
     gender = models.CharField(max_length=50, choices=GENDER, null=False, blank=False)
     date_of_birth = models.DateField()
-    email = models.EmailField(blank=False, null=False)
-    phone_number = models.CharField(max_length=15, validators=[RegexValidator(r'^\+?[0-9]{3} ?[0-9-]{8,11}$')])
+    email = models.EmailField(blank=False, null=False, unique=True)
+    phone_number = models.CharField(max_length=15, validators=[RegexValidator(r'^\+?[0-9]{3} ?[0-9-]{8,11}$')],
+                                    unique=True)
     state_of_origin = models.CharField(max_length=50, blank=False, null=False)
     state_of_residence = models.CharField(max_length=50, blank=False, null=False)
     city = models.CharField(max_length=50, blank=False, null=False)
     address_1 = models.CharField(max_length=200, blank=False, null=False)
     address_2 = models.CharField(max_length=200, blank=True, null=True)
     password = models.CharField(max_length=200, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True, default=timezone.now)
+    updated = models.DateTimeField(auto_now=True, default=timezone.now)
 
+    def __str__(self):
+        return self.full_name
