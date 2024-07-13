413db5c8b7e83b28943b554426eaa21b28444410
Victor Kevin <135155815+Kevinjnr@users.noreply.github.com>
2024-07-13 17:06:54 +0100

    update

--- estate_manage/company/forms.py
+++ estate_manage/company/forms.py
@@ -1,25 +1,12 @@
-from django import forms
 from django.forms import ModelForm
 from .models import *
 
 class CompanyForm(ModelForm):
-    class meta:
-        model = Company
-        fields = ['com_name', 'com_address', 'com_number', 'com_mail', 'com_web', 'com_cac',
-                   'com_logo', 'com_year_founded']
-        
-        labels = {
-            'com_name': 'Company Name', 
-            'com_address': 'Company address',
-            'com_number': 'Company number',
-            'com_mail': 'company Email',
-            'com_web': 'Company website',
-            'com_cac' : 'Company CAC',
-            'com_logo': 'Company logo',
-            'com_year_founded': 'Year founded'
-                 # Custom label for all field
-        }
+    class Meta:
+        model = Registration
+        fields = ['name', 'address', 'number', 'email', 'website', 'cac',
+                   'logo', 'year_founded']
 
         help_texts = {
-            'com_web':  ' (Optional)', 
+            'web':  ' (Optional)', 
         }
--- estate_manage/estate_manage/urls.py
+++ estate_manage/estate_manage/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('estate.urls'))
+    path("", include('estate.urls')),
+    path("", include('company.urls'))
 ]
