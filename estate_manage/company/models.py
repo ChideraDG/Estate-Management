798be1bdc7e101c2098eddc40768dc960e216efe
Treasurekeys <richedoriana7@gmail.com>
2024-07-13 12:34:06 +0100

    company models

--- estate_manage/company/models.py
+++ estate_manage/company/models.py
@@ -2,16 +2,16 @@ from django.db import models
 from django.core.validators import RegexValidator
 
 # Create your models here
-class Company(models.Model):
-    com_name= models.CharField(max_length = 100, blank= False, null= False)
-    com_address = models.TextField(max_length=200, blank=False, null=False)
-    com_number = models.CharField(max_length =15, unique=True, 
+class Registration(models.Model):
+    name= models.CharField(max_length = 100, blank= False, null= False)
+    address = models.TextField(max_length=200, blank=False, null=False)
+    number = models.CharField(max_length =15, unique=True, 
                                   validators=[RegexValidator(r'^\+?[0-9]{3} ?[0-9-]{8,11}$')])
-    com_mail = models.EmailField(unique=True, blank= False, null=False)
-    com_web = models.CharField(max_length=200, blank=True, null=False)
-    com_cac = models.CharField(max_length=200, blank= False, null=False)
-    com_logo = models.FileField(blank= False, null=False)
-    com_year_founded= models.CharField(max_length= 50, blank=False, null= False)
+    email = models.EmailField(unique=True, blank= False, null=False)
+    web = models.CharField(max_length=200, blank=True, null=False)
+    cac = models.CharField(max_length=200, blank= False, null=False)
+    logo = models.FileField(blank= False, null=False)
+    year_founded= models.CharField(max_length= 50, blank=False, null= False)
     
     def __str__(self):
-        return self.com_name
+        return self.email
