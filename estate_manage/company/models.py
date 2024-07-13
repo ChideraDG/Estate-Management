cb2a1919200aeb700310dd67da77e0918af33016
Chidera Divine-gift <chrischidera6@gmail.com>
2024-07-13 14:22:20 +0100

    Update models.py

--- estate_manage/company/models.py
+++ estate_manage/company/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from django.core.validators import RegexValidator
 
-# Create your models here
+
 class Registration(models.Model):
-    name= models.CharField(max_length = 100, blank= False, null= False)
+    name = models.CharField(max_length=100, blank=False, null=False)
     address = models.TextField(max_length=200, blank=False, null=False)
-    number = models.CharField(max_length =15, unique=True, 
-                                  validators=[RegexValidator(r'^\+?[0-9]{3} ?[0-9-]{8,11}$')])
-    email = models.EmailField(unique=True, blank= False, null=False)
-    web = models.CharField(max_length=200, blank=True, null=False)
-    cac = models.CharField(max_length=200, blank= False, null=False)
-    logo = models.FileField(blank= False, null=False)
-    year_founded= models.CharField(max_length= 50, blank=False, null= False)
-    
+    number = models.CharField(max_length=15, unique=True,
+                              validators=[RegexValidator(r'^\+?[0-9]{3} ?[0-9-]{8,11}$')])
+    email = models.EmailField(unique=True, blank=False, null=False)
+    website = models.CharField(max_length=200, blank=True, null=False)
+    cac = models.CharField(max_length=200, blank=False, null=False)
+    logo = models.FileField(blank=False, null=False)
+    year_founded = models.CharField(max_length=50, blank=False, null=False)
+
     def __str__(self):
         return self.email
