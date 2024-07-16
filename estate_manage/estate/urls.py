202269e8bc1c8132cb92371c5b48f36413650bbd
Maxc123456789 <chukwumamaxwell7@gmail.com>
2024-07-16 14:04:13 +0100

    update

--- estate_manage/estate/urls.py
+++ estate_manage/estate/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path("", welcome, name='welcome'),
-    path("reg/", userRegister, name='reg'),
+    path("register/", userRegister, name='register'),
     path("login/", userLogin, name='login'),
     path("logout/", userLogout, name='logout'),
     path("welcome/", test, name='welcome'),
