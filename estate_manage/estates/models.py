ea9ff1074e2b82d596be7c4e1ef9c0881aebc137
Chidera Divine-gift <chrischidera6@gmail.com>
2024-07-18 21:56:39 +0100

    update

--- estate_manage/estates/models.py
+++ estate_manage/estates/models.py
@@ -11,46 +11,11 @@ class Profile(models.Model):
         ('mixed_use', 'Mixed use'),
     ]
 
-    AMENITIES = [
-        ('parking', 'Parking'),
-        ('gym', 'Gym'),
-        ('swimming_pool', 'Swimming pool'),
-        ('cafeteria', 'Cafeteria'),
-        ('spa', 'Spa'),
-        ('playground', 'Playground'),
-        ('terraces', 'Terraces'),
-        ('helipads', 'Helipads'),
-        ('package_locker', 'Package locker'),
-        ('pet_friendly_amenities', 'Pet-Friendly Amenities'),
-        ('laundry_facilities', 'Laundry facilities'),
-    ]
-
-    SECURITY = [
-        ('perimeter_security', 'Perimeter Security'),
-        ('access_control_systems', 'Access Control Systems'),
-        ('surveillance_technology', 'Surveillance Technology'),
-        ('interior_security_measures', 'Interior Security Measures',),
-        ('balancing_visibility_and_discretion', 'Balancing Visibility and Discretion'),
-        ('regular_audits_and_assessments', 'Regular Audits and Assessments'),
-    ]
-
-    UTILITIES = [
-        ('electricity', 'Electricity'),
-        ('water', 'Water'),
-        ('gas', 'Gas'),
-        ('internet', 'Internet'),
-        ('sewage', 'Sewage'),
-        ('security', 'Security'),
-        ('elevator', 'Elevator'),
-        ('trash_collection', 'Trash Collection'),
-        ('garbage_collection', 'Garbage Collection'),
-     ]
-
     company = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True, null=True)
     estate_name = models.CharField(max_length=100, blank=False, null=False)
     estate_location = models.TextField(null=False, blank=False)
     estate_type = models.CharField(max_length=100, choices=DESIGNATION, blank=False, null=False)
-    estate_image = models.JSONField(default=list)  # JSONField for storing image paths
+    estate_image = models.JSONField(default=list)
     year_built = models.IntegerField(null=False, blank=False, validators=[MinValueValidator(1900)])
     number_of_houses = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(0)])
     number_of_apartments = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(0)])
@@ -66,7 +31,7 @@ class Profile(models.Model):
                                            validators=[MinValueValidator(0)])
     security_features = models.TextField(null=True, blank=True, default='')
     utility = models.TextField(null=True, blank=True, default='')
-    current_occupancy = models.IntegerField( null=True, blank=True, validators=[MinValueValidator(0)])
+    current_occupancy = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(0)])
     vacancy_rate = models.FloatField(null=True, blank=True, validators=[MinValueValidator(0)])
     estate_description = models.TextField(null=True, blank=True,)
     created = models.DateTimeField(auto_now_add=True)
