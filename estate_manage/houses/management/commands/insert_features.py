b372bd1df21b31212ef10f665280e2a05cb59bc6
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-09 11:53:31 +0100

    "Modified insert_features.py to update success message, deleted insert_utilities.py"

--- estate_manage/houses/management/commands/insert_features.py
+++ estate_manage/houses/management/commands/insert_features.py
@@ -31,4 +31,4 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         for code, name in self.FEATURES:
             Feature.objects.get_or_create(code=code, name=name)
-        self.stdout.write(self.style.SUCCESS('Successfully inserted features'))
+        self.stdout.write(self.style.SUCCESS('Successfully inserted house features'))
