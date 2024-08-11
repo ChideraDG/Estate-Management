899f70b52f7d104a0c40bca7e19fdca6f9bf289d
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-11 08:40:07 +0100

    Alignment change

--- estate_manage/tenants/models.py
+++ estate_manage/tenants/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from locations.models import Country, State
 from django.core.validators import MinValueValidator
 from apartments.models import Apartment
@@ -85,4 +86,3 @@ class Tenant(models.Model):
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
-                                                
