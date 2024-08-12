2ae7b8eda3dc02d993f22c92f56d314eeb1120b0
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-12 19:59:19 +0100

    "Removed import of 'reverse' from django.urls in tenants models"

--- estate_manage/tenants/models.py
+++ estate_manage/tenants/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.urls import reverse
 from locations.models import Country, State
 from django.core.validators import MinValueValidator
 from apartments.models import Apartment
