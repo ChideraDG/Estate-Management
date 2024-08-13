85922d43d6185a91baf178228ef6c6ad4a0069e9
Victor Kevin <135155815+Kevinjnr@users.noreply.github.com>
2024-08-13 17:17:03 +0100

    update

--- estate_manage/building_owners/apps.py
+++ estate_manage/building_owners/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class BuildingOwnersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "building_owners"
+
+    def ready(self):
+        import building_owners.signals  
