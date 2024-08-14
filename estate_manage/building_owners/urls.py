00f72908f91d118d626dd3fd041b37fc1c8f9239
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-14 14:44:06 +0100

    "Updated URL pattern for building owner dashboard"

--- estate_manage/building_owners/urls.py
+++ estate_manage/building_owners/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('get_states/', get_states, name='get-states'),
     path('dashboard/<str:pk>/update-bo-profile/', updateProfile, name='update-building-owner'),
     path('dashboard/<str:pk>/view-bo-profile/', viewProfile, name='view-building-owner'),
-    path('<str:pk>/', buildingOwnerDashboard, name="dashboard-BO"),
+    path('dashboard/<str:pk>/', buildingOwnerDashboard, name="dashboard-BO"),
 ]
