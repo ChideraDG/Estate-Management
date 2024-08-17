58265f2b70897ed52e1fdfd38348908104c10e0e
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-17 21:05:13 +0100

    Deleted the update building owner url

--- estate_manage/building_owners/urls.py
+++ estate_manage/building_owners/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from .views import (get_states, view_building_owner_profile,
-                    update_building_owner_profile, building_owner_dashboard,
-                    view_connections)
+                    building_owner_dashboard, view_connections)
 
 urlpatterns = [
     path('get_states/', get_states, name='get-states'),
-    path('dashboard/<str:pk>/update-bo-profile/', update_building_owner_profile, name='update-building-owner'),
     path('dashboard/<str:pk>/view-bo-profile/', view_building_owner_profile, name='view-building-owner'),
     path('dashboard/bo/<str:pk>/', building_owner_dashboard, name="dashboard-BO"),
     path('dashboard/<str:pk>/view-connections/', view_connections, name="view-connections")
