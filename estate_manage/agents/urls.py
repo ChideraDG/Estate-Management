2d129bd58640b68061b2a61b8c47d47756a8713a
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-18 16:06:31 +0100

    "Worked on the Agent url"

--- estate_manage/agents/urls.py
+++ estate_manage/agents/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("<str:pk>/", agentsDashboard, name='dashboard-A'),
+    path("A/<str:pk>/", agentsDashboard, name='dashboard-A'),
 ]
