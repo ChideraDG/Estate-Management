5f402207642d8fc314b1299ddbb7125c39f80b18
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-19 11:49:36 +0100

    removed the biilding owner houses from the urls

--- estate_manage/building_owners/urls.py
+++ estate_manage/building_owners/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from .views import (get_states, building_owner_profile,
-                    building_owner_dashboard, view_connections,
-                    building_owner_houses)
+                    building_owner_dashboard, view_connections)
 
 urlpatterns = [
     path('get_states/', get_states, name='get-states'),
     path('bo/<str:pk>/bo-profile/', building_owner_profile, name='view-building-owner'),
     path('bo/<str:pk>/', building_owner_dashboard, name="dashboard-BO"),
     path('bo/<str:pk>/view-connections/', view_connections, name="view-connections"),
-    path('bo/<str:pk>/houses/', building_owner_houses, name='bo-houses'),
 ]
