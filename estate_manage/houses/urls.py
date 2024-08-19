e90b1259e89b8db9223225047e5502860b484f61
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-19 11:51:57 +0100

    Removed unused urls from the houses url

--- estate_manage/houses/urls.py
+++ estate_manage/houses/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('house-home/', houseHome, name='house-home'),
     path('get_states/', get_states, name='get-states'),
-    path('create/', createHouse, name='create-house'),
-    path('update/<str:pk>/', updateHouse, name='update-house'),
-    path('view/<str:pk>/', viewHouse, name='view-house'),
-    path('delete/<str:pk>/', deleteHouse, name='delete-house'),
+    path('bo/<str:pk>/houses/', building_owner_houses, name='bo-houses'),
 ]
