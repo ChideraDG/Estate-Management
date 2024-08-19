ff971710b6264c5a0a411759189dd496b3000375
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-19 20:32:16 +0100

    add a new utils file to handdle paginations

---
+++ estate_manage/houses/utils.py
@@ -0,0 +1,20 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+
+def paginateHouses(request, houses, data_num):
+    page = request.GET.get('page')
+    paginator = Paginator(houses, data_num)
+
+    try:
+        houses = paginator.page(page)
+    except PageNotAnInteger:
+        houses = paginator.page(1)
+    except EmptyPage:
+        houses = paginator.page(paginator.num_pages)
+
+    leftIndex = max(1, int(page) - 1)
+    rightIndex = min(paginator.num_pages + 1, int(page) + 5)
+
+    custom_range = range(leftIndex, rightIndex)
+
+    return custom_range, houses
