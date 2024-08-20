a1887f17fdcb2cdb95e09db85c7d72ef943af6c3
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-20 10:46:03 +0100

    Added signals to the houses to increase portfolio size after creating a new house

---
+++ estate_manage/houses/signals.py
@@ -0,0 +1,21 @@
+from django.db.models.signals import post_save, pre_delete
+from django.dispatch import receiver
+from .models import House
+from building_owners.models import BuildingOwner
+
+
+@receiver(post_save, sender=House)
+def add_portfolio_size(sender, instance, created, **kwargs):
+    if created:
+        if instance.building_owner:
+            bo_user = BuildingOwner.objects.get(id=instance.building_owner.id)
+            bo_user.portfolio_size += 1
+            bo_user.save()
+
+
+@receiver(pre_delete, sender=House)
+def delete_portfolio_size(sender, instance, **kwargs):
+    if instance.building_owner:
+        bo_user = BuildingOwner.objects.get(id=instance.building_owner.id)
+        bo_user.portfolio_size -= 1
+        bo_user.save()
