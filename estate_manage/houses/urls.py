aa35c3fc080af4dc03e6704d180dd6f0576c8161
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-22 15:49:34 +0100

    Changed the url path for the houses

--- estate_manage/houses/urls.py
+++ estate_manage/houses/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import get_states, building_owner_houses, house_details, delete_house
+from .views import get_states, houses, house_details, delete_house
 
 urlpatterns = [
     path('get_states/', get_states, name='get-states'),
-    path('bo/<str:pk>/houses/', building_owner_houses, name='bo-houses'),
-    path('bo/<str:pk>/houses/<str:house_id>/', house_details, name="house-details"),
+    path('<str:type>/<str:pk>/houses/', houses, name='houses'),
+    path('<str:type>/<str:pk>/houses/<str:house_id>/', house_details, name="house-details"),
     path('delete-house/<str:pk>/', delete_house, name="delete-house"),
 ]
