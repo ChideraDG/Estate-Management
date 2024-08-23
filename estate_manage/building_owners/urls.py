b92fb7c08925e386120572356a9c561385306e8b
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-23 18:48:08 +0100

    fixed the name of a url to avoid conflict

--- estate_manage/building_owners/urls.py
+++ estate_manage/building_owners/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('get_states/', get_states, name='get-states'),
     path('bo/<str:pk>/bo-profile/', building_owner_profile, name='view-building-owner'),
     path('bo/<str:pk>/', building_owner_dashboard, name="dashboard-BO"),
-    path('bo/<str:pk>/view-connections/', view_connections, name="view-connections"),
+    path('bo/<str:pk>/view-connections/', view_connections, name="bo-view-connections"),
 ]
