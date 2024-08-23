73282ac154c706ed953f532105faa14b6393894d
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-23 19:54:00 +0100

    added delete route and apartment details route

--- estate_manage/apartments/urls.py
+++ estate_manage/apartments/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import house_apartments, view_apartments
+from .views import house_apartments, view_apartments, apartment_details, delete_apartment
 
 urlpatterns = [
     path('<str:type>/<str:pk>/apartment-control/', house_apartments, name='house-apartments'),
     path('<str:type>/<str:pk>/<str:house_id>/apartments/', view_apartments, name='view-apartments'),
+    path('<str:type>/<str:pk>/apartments/<str:house_id>/<str:apartment_number>/', apartment_details, name="apartment-details"),
+    path('delete/<str:pk>/', delete_apartment, name="delete-apartment")
 ]
