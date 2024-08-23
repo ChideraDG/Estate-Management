a392b84091319f77ef04e9a3d888369e4111eb7a
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-23 15:32:00 +0100

    Added filter apartments to the apartments form

--- estate_manage/apartments/views.py
+++ estate_manage/apartments/views.py
@@ -1,10 +1,12 @@
+from urllib.parse import urlencode
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.contrib import messages
 from houses.models import House
+from .utils import paginateApartments
 from .models import Apartment, Photo
-from .forms import ApartmentForm
+from .forms import ApartmentForm, ApartmentFilterForm
 
 
 def createApartment(request):
@@ -69,11 +71,18 @@ def view_apartments(request, type, pk, house_id):
     active_menu = request.GET.get('active_menu', '/')
     active_sub_menu = request.GET.get('active_sub_menu', '/')
     menu = request.GET.get('menu', 'all')
+    reset_filter = request.GET.get('reset_filter', '/')
     house = House.objects.get(id=house_id)
-    apartments = house.apartments.all()
-    occupied_apartments = [ apartment for apartment in apartments if apartment.is_occupied ]
-    vacant_apartments = [ apartment for apartment in apartments if not apartment.is_occupied ]
-    exist = [apartments.exists(), len(occupied_apartments), len(vacant_apartments)]
+    apartments, query_string = filterApartments(request, house)
+    total_apartment = apartments.count
+    occupied_apartments = apartments.filter(is_occupied=1)
+    vacant_apartments = apartments.filter(is_occupied=0)
+    exist = [apartments.exists(), occupied_apartments.exists(), len(vacant_apartments)]
+    custom_range, apartments = paginateApartments(request, apartments, 6)
+    oa_custom_range, occupied_apartments = paginateApartments(request, occupied_apartments, 6)
+    va_custom_range, vacant_apartments = paginateApartments(request, vacant_apartments, 6)
+    filter_form = ApartmentFilterForm()
+    print(occupied_apartments, apartments)
 
     if request.method == 'POST':
         form = ApartmentForm(request.POST, request=request)
@@ -115,5 +124,60 @@ def view_apartments(request, type, pk, house_id):
         'occupied_apartments': occupied_apartments,
         'vacant_apartments': vacant_apartments,
         'exist': exist,
+        'oa_custom_range': oa_custom_range,
+        'va_custom_range': va_custom_range,
+        'custom_range': custom_range,
+        'filter_form': filter_form,
+        'query_string': query_string,
+        'reset_filter': reset_filter,
+        'house_id': house_id,
+        'total_apartment': total_apartment,
     }
     return render(request, 'apartments/view_apartments.html', context)
+
+
+def filterApartments(request, house):
+    apartments = house.apartments.all()
+    form = ApartmentFilterForm(request.GET)
+
+    if form.is_valid():
+        filters = {}
+
+        # Define the fields that need direct filtering
+        fields_to_filters = {
+            'apartment_number': 'apartment_number__icontains',
+            'floor_number': 'floor_number',
+            'number_of_bedrooms': 'number_of_bedrooms',
+            'number_of_bathrooms': 'number_of_bathrooms',
+            'square_feet_min': 'square_feet__gte',
+            'square_feet_max': 'square_feet__lte',
+            'rent_price_min': 'rent_price__gte',
+            'rent_price_max': 'rent_price__lte',
+            'sale_price_min': 'sale_price__gte',
+            'sale_price_max': 'sale_price__lte',
+            'number_of_rooms': 'number_of_rooms',
+            'kitchen_type': 'kitchen_type',
+            'flooring_type': 'flooring_type',
+            'heating_system': 'heating_system',
+            'security_deposit': 'security_deposit__gte',
+            'maintenance_fee': 'maintenance_fee__gte',
+            'last_renovation_year': 'last_renovation_year',
+            'condition': 'condition__icontains',
+        }
+
+        # Loop through the fields and add non-empty filters
+        for field, filter_name in fields_to_filters.items():
+            value = form.cleaned_data.get(field)
+            if value:
+                filters[filter_name] = value
+
+        # Apply the filters to the queryset
+        apartments = apartments.filter(**filters)
+
+        # Generate the query string for the current GET parameters
+        cleaned_query_dict = {key: value for key, value in request.GET.items() if value}
+        query_string = urlencode(cleaned_query_dict)
+    else:
+        query_string = ""
+
+    return apartments, query_string
