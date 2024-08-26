a9db2a024554bcd4045c3852599456aa312ad975
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-26 16:50:07 +0100

    created agent_profile view

--- estate_manage/agents/views.py
+++ estate_manage/agents/views.py
@@ -1,7 +1,40 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.contrib import messages
+from locations.models import Country, State
 from .models import Agent
+from  .forms import AgentForm
 
+
+@login_required(login_url='login')
+def agent_profile(request, pk):
+    profile = request.user.profile.agents
+    countries = Country.objects.all()
+
+    if request.method == "POST":
+        form  = AgentForm(request.POST, request.FILES, instance=profile)
+    else:
+        form = AgentForm(instance=profile)
+
+    active_menu = 'user-management'
+    active_sub_menu = 'a-profile'
+    
+    context = {
+        'active_sub_menu': active_sub_menu,
+        'active_menu': active_menu,
+        'profile': profile,
+        'form': form, 
+        'countries': countries, 
+    }
+    return render(request, "agents/agent_profile.html", context)
+
+@login_required(login_url='login')
+def get_states(request):
+    country_id = request.GET.get('country_id')
+    states = State.objects.filter(country_id=country_id).order_by('name')
+    states_list = [{'id': state.id, 'name': state.name} for state in states]
+    return JsonResponse(states_list, safe=False)
 
 @login_required(login_url='login')
 def agentsDashboard(request, pk):
