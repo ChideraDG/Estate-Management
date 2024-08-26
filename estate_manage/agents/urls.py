6b273b44f50c1cbe0dfb567318f90c7b59e1cda9
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-26 16:02:56 +0100

    modified URLs for agents

--- estate_manage/agents/urls.py
+++ estate_manage/agents/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import *
+from .views import agentsDashboard, view_connections
 
 urlpatterns = [
     path("A/<str:pk>/", agentsDashboard, name='dashboard-A'),
+    path("A/<str:pk>/view-connections", view_connections, name="a-view-connections"),
 ]
