395a3c4d92e4a5686a0d704b1e7e057716504fae
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-26 16:03:34 +0100

    Added view_connections to Agent view

--- estate_manage/agents/views.py
+++ estate_manage/agents/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Agent
 
+
+@login_required(login_url='login')
 def agentsDashboard(request, pk):
     active_menu = "agent-dashboard"
     context = {
@@ -9,3 +12,23 @@ def agentsDashboard(request, pk):
     }
     return render(request, "agents/A_dashboard.html", context)
 
+@login_required(login_url='login')
+def view_connections(request, pk):
+    active_menu = 'user-management'
+    active_sub_menu = request.GET.get('active_sub_menu', 'personal-profile')
+    user_type = request.user.profile.designation
+    labels = {
+        "building_owner": 'BO',
+        "agent": 'A',
+        "buyer": 'B',
+        "company": 'C',
+        "tenant": 'T'
+    }
+
+
+    context = {
+        'active_sub_menu': active_sub_menu,
+        'active_menu': active_menu,
+        'type': labels.get(user_type)
+    }
+    return render(request, "building_owners/view_connections.html", context)
