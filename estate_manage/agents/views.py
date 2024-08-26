485ebc8028098740ed3dfbcdc4cffc29683a0efe
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-26 15:34:03 +0100

    updated agentsDashboard view in agents app

--- estate_manage/agents/views.py
+++ estate_manage/agents/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Agent
 
 def agentsDashboard(request, pk):
-    context = {'username': pk}
+    active_menu = "agent-dashboard"
+    context = {
+        'username': pk,
+        'active_menu': active_menu,
+    }
     return render(request, "agents/A_dashboard.html", context)
 
