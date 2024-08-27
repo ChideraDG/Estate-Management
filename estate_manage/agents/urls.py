30127e8a04b611b9f70169e8e825a40842f68c23
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-27 14:24:19 +0100

    Update urls.py

--- estate_manage/agents/urls.py
+++ estate_manage/agents/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import (agentsDashboard, view_connections, agent_profile,
-                    my_properties)
+                    agent_properties, agent_house_apartments)
 
 urlpatterns = [
     path("A/<str:pk>/", agentsDashboard, name='dashboard-A'),
     path("A/<str:pk>/view-connections", view_connections, name="a-view-connections"),
     path('A/<str:pk>/a-profile/', agent_profile, name='view-agent'),
-    path("A/<str:pk>/my-properties/", my_properties, name='my-properties'),
+    path("A/<str:pk>/properties/", agent_properties, name='agent-properties'),
+    path("A/<str:pk>/properties/<str:p_id>/", agent_house_apartments, name='agent-house-apartments')
 ]
