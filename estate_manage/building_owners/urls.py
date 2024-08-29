4b06485615006aca373e2081f3914f0f2a28770f
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-29 19:34:39 +0100

    Update urls.py

--- estate_manage/building_owners/urls.py
+++ estate_manage/building_owners/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from .views import (get_states, building_owner_profile, get_apartments,
-                    building_owner_dashboard, view_connections, tenant_profiles)
+from .views import (get_states, building_owner_profile,
+                    building_owner_dashboard, view_connections)
 
 urlpatterns = [
     path('get_states/', get_states, name='get-states'),
-    path('get_apartments/', get_apartments, name='get-apartments'),
     path('bo/<str:pk>/bo-profile/', building_owner_profile, name='view-building-owner'),
     path('bo/<str:pk>/', building_owner_dashboard, name="dashboard-BO"),
     path('bo/<str:pk>/view-connections/', view_connections, name="bo-view-connections"),
-    path('bo/<str:pk>/tenant-profiles/', tenant_profiles, name='bo-tenant-profiles')
 ]
