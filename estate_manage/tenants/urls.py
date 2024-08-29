f6b0f8c9952bf586ec3526a45d19ebeacef1ce9c
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-29 19:34:14 +0100

    Update urls.py

--- estate_manage/tenants/urls.py
+++ estate_manage/tenants/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import (get_states, view_connections, tenantDashboard,
-                                tenant_profile)
+                                tenant_profile, tenants_profiles)
 
 urlpatterns = [
     path('get_states/', get_states, name='get-states'),
     path('tenant/<str:pk>/t-profile/', tenant_profile, name='view-tenant'),
     path('tenant/<str:pk>/view-connections/', view_connections, name='t-view-connections'),
     path('tenant/<str:pk>/', tenantDashboard, name='dashboard-T'),
+    path('<str:type>/<str:pk>/tenant-profiles/', tenants_profiles, name='tenants-profiles')
 ]
