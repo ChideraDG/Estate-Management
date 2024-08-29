f7afc19685a8b9ff93e4069da326e2c1f610a897
Chidera Divine-gift <chrischidera6@gmail.com>
2024-08-29 19:34:08 +0100

    Update urls.py

--- estate_manage/apartments/urls.py
+++ estate_manage/apartments/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import house_apartments, view_apartments, apartment_details, delete_apartment
+from .views import house_apartments, view_apartments, apartment_details, delete_apartment, get_apartments
 
 urlpatterns = [
+    path('get_apartments/', get_apartments, name='get-apartments'),
     path('<str:type>/<str:pk>/apartment-control/', house_apartments, name='house-apartments'),
     path('<str:type>/<str:pk>/<str:house_id>/apartments/', view_apartments, name='view-apartments'),
     path('<str:type>/<str:pk>/apartments/<str:house_id>/<str:apartment_number>/', apartment_details, name="apartment-details"),
-    path('delete/<str:pk>/', delete_apartment, name="delete-apartment")
+    path('delete/<str:pk>/', delete_apartment, name="delete-apartment"),
 ]
