6100407c4cb4772f6d6faccaee08a3c91936ff31
Chidera Divine-gift <chrischidera6@gmail.com>
2024-09-03 17:41:51 +0100

    Update views.py

--- estate_manage/leaseAgreements/views.py
+++ estate_manage/leaseAgreements/views.py
@@ -5,21 +5,23 @@ from .forms import LeaseAgreementForm
 def lease_agreements(request, type, pk):
     active_menu = request.GET.get("active_menu", "")
     active_sub_menu = request.GET.get("active_sub_menu", "")
+    lease_update = request.GET.get("lease_update", False)
+    tenant = request.GET.get("tenant_id", "")
+    lease_agreement = LeaseAgreement.objects.filter(tenant=tenant).first()
     profile = request.user.profile
-
     tenants = None
     if profile.designation == "building_owner":
         tenants = profile.building_owner.tenants.all()
         agreements = LeaseAgreement.objects.filter(tenant__in=tenants)
 
     if request.method == "POST":
-        form = LeaseAgreementForm(request.POST, building_owner=profile.building_owner)
+        form = LeaseAgreementForm(request.POST, request.FILES, instance=lease_agreement)
         if form.is_valid():
-            form.save()
+            agreement = form.save()
         else:
             pass
     else:
-        form = LeaseAgreementForm(building_owner=profile.building_owner)
+        form = LeaseAgreementForm(instance=lease_agreement)
 
     template_routes = {
         'building_owner': "building_owners/BO_dashboard.html",
@@ -31,5 +33,6 @@ def lease_agreements(request, type, pk):
         "agreements": agreements,
         "tenants": tenants,
         "form": form,
+        "lease_update": lease_update,
     }
     return render(request, 'leaseAgreements/agreements.html', context)
