22abc3026fbf5ff117bba92cbb646b250f53a727
Chidera Divine-gift <chrischidera6@gmail.com>
2024-09-04 17:29:23 +0100

    Update views.py

--- estate_manage/leaseAgreements/views.py
+++ estate_manage/leaseAgreements/views.py
@@ -50,7 +50,26 @@ def lease_agreements(request, type, pk):
         "template_routes": template_routes.get(profile.designation),
         "agreements": agreements,
         "tenants": tenants,
+        "type": type,
         "form": form,
         "lease_update": lease_update,
     }
     return render(request, 'leaseAgreements/agreements.html', context)
+
+def agreements_details(request, type, pk, agreement_id):
+    menu = request.GET.get("menu", "/")
+    s_menu = request.GET.get("s_menu", "/")
+    profile = request.user.profile
+    agreement = LeaseAgreement.objects.get(id=agreement_id)
+
+    template_routes = {
+        'building_owner': "building_owners/BO_dashboard.html",
+    }
+    context = {
+        'menu': menu,
+        's_menu': s_menu,
+        'agreement': agreement,
+        'type': type,
+        'template_routes': template_routes.get(profile.designation),
+    }
+    return render(request, 'leaseAgreements/agreements_details.html', context)
