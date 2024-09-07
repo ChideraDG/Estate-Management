f5cf1c2872742ba73ba00fd1ba7b4ee7f3697034
Chidera Divine-gift <chrischidera6@gmail.com>
2024-09-07 20:00:57 +0100

    Update urls.py

--- estate_manage/leaseAgreements/urls.py
+++ estate_manage/leaseAgreements/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('<type>/<pk>/agreements/', views.lease_agreements, name='agreements'),
     path('<type>/<pk>/agreements/<agreement_id>/', views.agreements_details, name='agreement-detail'),
+    path('<type>/<pk>/agreements/<agreement_id>/update/', views.update_agreement, name='update-agreement'),
 ]
