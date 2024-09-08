aa474266383ac4d84069544e9197502f8454a15d
Chidera Divine-gift <chrischidera6@gmail.com>
2024-09-08 12:08:20 +0100

    Update views.py

--- estate_manage/communications/views.py
+++ estate_manage/communications/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.db.models import Q
+from .models import Message, Conversation
 
-# Create your views here.
+
+def bo_tenant_communications(request, pk):
+    all_messages = Message.objects.filter(Q(sender=request.user) |
+                                  Q(recipient=request.user))
+    unread_messages = all_messages.filter(is_read=False)
+    read_messages = all_messages.filter(is_read=True)
+    conversations = Conversation.objects.filter(user=request.user).order_by('-latest_message_timestamp')
+
+    context = {
+        "menu": 'tm',
+        "s_menu": 'ct',
+        "all_messages": all_messages,
+        "unread_messages": unread_messages,
+        "read_messages": read_messages,
+        "conversations": conversations,
+    }
+    return render(request, 'communications/bo_comms.html', context)
