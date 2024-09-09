32e1513ee7e13b07bdc115026c657b8f81e269ca
Chidera Divine-gift <chrischidera6@gmail.com>
2024-09-09 19:01:54 +0100

    Update settings.py

--- estate_manage/estate_manage/settings.py
+++ estate_manage/estate_manage/settings.py
@@ -94,6 +94,9 @@ DATABASES = {
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',  # This ensures Django communicates using utf8mb4
+        },
     }
 }
 
