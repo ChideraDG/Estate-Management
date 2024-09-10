056e2489a80c96c8aed5ecbcd1060d4cf6e6a664
Chidera Divine-gift <chrischidera6@gmail.com>
2024-09-10 19:56:01 +0100

    Update urls.py

--- estate_manage/communications/urls.py
+++ estate_manage/communications/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .bo_views import bo_tenant_communications, search_clients, bo_chat
+from .bo_views import bo_tenant_communications, search_clients, bo_chat, search_chats
 
 
 urlpatterns = [
     path('search_clients/', search_clients, name='search_clients'),
+    path('search_chats/', search_chats, name='search_chats'),
     path("bo/<pk>/communications/", bo_tenant_communications, name="bo-comms"),
     path("bo/<pk>/communications/<tenant_id>/", bo_chat, name='bo-chat'),
 ]
