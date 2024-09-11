8b961604015cc22e44f2fee9d90fc9e7deac7388
Chidera Divine-gift <chrischidera6@gmail.com>
2024-09-11 17:17:45 +0100

    Update signals.py

--- estate_manage/communications/signals.py
+++ estate_manage/communications/signals.py
@@ -2,11 +2,16 @@ from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
 from django.utils.timezone import now
 from building_owners.models import BuildingOwner
-from . import bo_view
+from users.models import Profile
+from . import bo_view, tenant_view
 
 @receiver(user_logged_in)
 def log_user_login(sender, request, user, **kwargs):
-    if user.profile.designation == 'building owner':
+    if user.profile.designation == 'building_owner':
         # The total unread messages across all tenants
-        unread_total = bo_view.get_unread(request)
-        BuildingOwner.objects.filter(id=request.user.profile.building_owner.id).update(unread_messages=unread_total)
+        unread_total = bo_view.bo_get_unread(request)
+        Profile.objects.filter(username=request.user).update(unread_messages=unread_total)
+    elif user.profile.designation == 'tenant':
+        if user.profile.tenant.building_owner:
+            unread_total = tenant_view.tenant_get_unread(request)
+            Profile.objects.filter(username=request.user).update(unread_messages=unread_total)  
