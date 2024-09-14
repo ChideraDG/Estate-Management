fdfe64446384c2a238a3f2dc59817b78ba0b1535
Kevinjnr <vikzilla.28@gmail.com>
2024-09-14 14:08:21 +0100

    update

--- estate_manage/estates/urls.py
+++ estate_manage/estates/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import (get_states, estates)
+from .views import (get_states, estates, deleteEstate)
 
 urlpatterns = [
     path('get-states', get_states, name='get-states'),
-     path('<str:type>/<str:pk>/estates/', estates, name='estates'),
+    path('<str:type>/<str:pk>/estates/', estates, name='estates'),
+    # path('<str:type>/<str:pk>/estates/<str:estate_id>/', estate_details, name="estate-details"),
+    path('delete-estate/<str:pk>/', deleteEstate, name="delete-estate"),
 ]
