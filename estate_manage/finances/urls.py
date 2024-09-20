f9efc91280e9db12e66c8badd7facb05a158b879
Chidera Divine-gift <chrischidera6@gmail.com>
2024-09-20 19:40:38 +0100

    Update urls.py

--- estate_manage/finances/urls.py
+++ estate_manage/finances/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import (rent_payment_portal, generate_rent_receipt)
 
 urlpatterns = [
-    path("tenant/<pk>/genarate_rent_receipt/", generate_rent_receipt, name="generate_rent_receipt"),
+    path("<pk>/genarate_rent_receipt/", generate_rent_receipt, name="generate_rent_receipt"),
     path("tenant/<pk>/rent-payment/", rent_payment_portal, name="rent-payment"),
 ]
