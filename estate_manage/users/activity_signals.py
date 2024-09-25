bbcb51c78e598b4ebfeb3d8f35183c93971939bd
Chidera Divine-gift <chrischidera6@gmail.com>
2024-09-25 19:25:42 +0100

    Update activity_signals.py

--- estate_manage/users/activity_signals.py
+++ estate_manage/users/activity_signals.py
@@ -1,6 +1,7 @@
 from django.dispatch import receiver
 from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.db.models.signals import post_save
+from finances.models import RentPayment
 from .models import ActivityLog
 
 
@@ -10,7 +11,7 @@ def user_login(sender, request, user, **kwargs):
         user=user,
         action_type="Login",
         colour="success",
-        description=f"{user.username} logged in.",
+        description=f"{user.username} logged in",
     )
 
 @receiver(signal=user_logged_out)
@@ -19,5 +20,25 @@ def user_logout(sender, request, user, **kwargs):
         user=user,
         action_type="Logout",
         colour="danger",
-        description=f"{user.username} logged out.",
+        description=f"{user.username} logged out",
     )
+
+@receiver(post_save, sender=RentPayment)
+def rent_payment(sender, instance, created, **kwargs):
+    if created:
+        ActivityLog.objects.create(
+        user=instance.lease.tenant.user.user,
+        entity_type="Finance",
+        entity_id=instance.id,
+        colour="primary",
+        description=f"Made a Rent Payment",
+        )
+
+        ActivityLog.objects.create(
+        user=instance.lease.tenant.building_owner.user.user,
+        entity_type="Finance",
+        entity_id=instance.id,
+        colour="primary",
+        description=f"Received a Rent Payment",
+        )
+    
