e858845e594cef0ccf4075932757dbf2bc37bc6b
Chidera Divine-gift <chrischidera6@gmail.com>
2024-10-23 22:42:20 +0100

    Update activity_signals.py

--- estate_manage/users/activity_signals.py
+++ estate_manage/users/activity_signals.py
@@ -1,6 +1,6 @@
 from django.dispatch import receiver
 from django.contrib.auth.signals import user_logged_in, user_logged_out
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from finances.models import RentPayment
 from houses.models import House
 from apartments.models import Apartment
@@ -51,12 +51,28 @@ def add_house(sender, instance, created, **kwargs):
     if created:
         ActivityLog.objects.create(
         user=instance.building_owner.user.user if instance.building_owner else instance.estate.company.user.user,
+        action_type="Create",
         entity_type="House",
         entity_id=instance.id,
         colour="dark",
         description=f"Added a House",
         ip_address="house-details"
         )
+
+@receiver(pre_delete, sender=House)
+def delete_house(sender, instance, **kwargs):
+    del_instance = ActivityLog.objects.filter(entity_id=instance.id, entity_type="House").last()
+    del_instance.ip_address = None
+    del_instance.save()
+
+    ActivityLog.objects.create(
+        user=instance.building_owner.user.user if instance.building_owner else instance.estate.company.user.user,
+        action_type="Delete",
+        entity_type="House",
+        entity_id=instance.id,
+        colour="error",
+        description=f"Deleted a House",
+    )
 
 @receiver(post_save, sender=Apartment)
 def add_apartment(sender, instance, created, **kwargs):
