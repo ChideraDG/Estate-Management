9c2d2cdfd21b968bcffec1bf6091ee3367f4faf9
Chidera Divine-gift <chrischidera6@gmail.com>
2024-10-24 15:31:51 +0100

    "Added pagination to requests view and template, refactored request listing template to use includes and pagination"

--- estate_manage/maintenances/bo_views.py
+++ estate_manage/maintenances/bo_views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import WorkOrder
+from .utils import paginateRequest
 
 
 def requests(request, pk):
@@ -19,13 +21,20 @@ def requests(request, pk):
                     completed_requests.append(log)
                 elif log.status == 'closed':
                     closed_requests.append(log)
+
+    open = paginateRequest(request, sorted(open_requests, key=lambda x: x.reported_date, reverse=True), 6)
+    in_progress = paginateRequest(request, sorted(in_progress_requests, key=lambda x: x.reported_date, reverse=True), 6)
+    completed = paginateRequest(request, sorted(completed_requests, key=lambda x: x.reported_date, reverse=True), 6)
     
     context = {
         'menu': 'mt',
         's_menu': 'm-r',
-        'open': sorted(open_requests, key=lambda x: x.reported_date, reverse=True),
-        'in_progress': sorted(in_progress_requests, key=lambda x: x.reported_date, reverse=True),
-        'completed': sorted(completed_requests, key=lambda x: x.reported_date, reverse=True),
+        'open': open[1],
+        'open_index': open[0],
+        'in_progress': in_progress[1],
+        'in_progress_index': in_progress[0],
+        'completed': completed[1],
+        'completed_index': completed[0],
         'closed': sorted(closed_requests, key=lambda x: x.reported_date, reverse=True),
         'open_count': len(open_requests),
         'in_progress_count': len(in_progress_requests),
