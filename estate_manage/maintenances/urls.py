2c309e5546fddcc20aa5d760a35b32bf701ba237
Chidera Divine-gift <chrischidera6@gmail.com>
2024-10-24 19:09:50 +0100

    Update urls.py

--- estate_manage/maintenances/urls.py
+++ estate_manage/maintenances/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .tenants_views import (tracking, request_submission, service_provider)
-from .bo_views import (requests)
+from .bo_views import (requests, cancel_request, reopen_request, delete_request,
+                       completed_request)
 
 urlpatterns = [
     # tenants work-order urls
@@ -10,4 +11,8 @@ urlpatterns = [
 
     # building owner requests urls
     path('bo/<pk>/maintenance/requests/', requests, name='requests'),
+    path('cancel-request/<pk>/', cancel_request, name='cancel_request'),
+    path('reopen-request/<pk>/', reopen_request, name='reopen_request'),
+    path('delete-request/<pk>/', delete_request, name='delete_request'),
+    path('completed-request/<pk>/', completed_request, name='completed_request'),
 ]
