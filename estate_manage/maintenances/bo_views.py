c84f52d929a15f8ae793a8be4a448e58bb66a16d
Chidera Divine-gift <chrischidera6@gmail.com>
2024-10-24 19:10:06 +0100

    Update bo_views.py

--- estate_manage/maintenances/bo_views.py
+++ estate_manage/maintenances/bo_views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import WorkOrder
 from .utils import paginateRequest
 
 
+@login_required(login_url='login')
 def requests(request, pk):
-    section = request.GET.get('section', '')
+    i_menu = request.GET.get('i_menu', 'open')
     tenants = request.user.profile.building_owner.tenants.all()
     open_requests, in_progress_requests, completed_requests, closed_requests = [], [], [], []
 
@@ -22,9 +25,10 @@ def requests(request, pk):
                 elif log.status == 'closed':
                     closed_requests.append(log)
 
-    open = paginateRequest(request, sorted(open_requests, key=lambda x: x.reported_date, reverse=True), 6)
-    in_progress = paginateRequest(request, sorted(in_progress_requests, key=lambda x: x.reported_date, reverse=True), 6)
-    completed = paginateRequest(request, sorted(completed_requests, key=lambda x: x.reported_date, reverse=True), 6)
+    open = paginateRequest(request, sorted(open_requests, key=lambda x: x.reported_date, reverse=True), 5)
+    in_progress = paginateRequest(request, sorted(in_progress_requests, key=lambda x: x.reported_date, reverse=True), 5)
+    completed = paginateRequest(request, sorted(completed_requests, key=lambda x: x.reported_date, reverse=True), 5)
+    closed = paginateRequest(request, sorted(closed_requests, key=lambda x: x.reported_date, reverse=True), 5)
     
     context = {
         'menu': 'mt',
@@ -35,11 +39,50 @@ def requests(request, pk):
         'in_progress_index': in_progress[0],
         'completed': completed[1],
         'completed_index': completed[0],
-        'closed': sorted(closed_requests, key=lambda x: x.reported_date, reverse=True),
+        'closed': closed[1],
+        'closed_index': closed[0],
         'open_count': len(open_requests),
         'in_progress_count': len(in_progress_requests),
         'completed_count': len(completed_requests),
         'closed_count': len(closed_requests),
-        'section': section,
+        'i_menu': i_menu,
     }
     return render(request, 'building_owner_maintain/requests.html', context)
+
+@login_required(login_url='login')
+def cancel_request(request, pk):
+    instance = WorkOrder.objects.get(id=pk)
+    instance.status = 'closed'
+    instance.reported_date =  timezone.now()
+    instance.save()
+
+    messages.success(request, f"Request with ID={pk} was cancelled.")
+    return redirect("requests", pk=request.user.profile)
+
+@login_required(login_url='login')
+def reopen_request(request, pk):
+    instance = WorkOrder.objects.get(id=pk)
+    instance.status = 'open'
+    instance.reported_date =  timezone.now()
+    instance.save()
+
+    messages.success(request, f"Request with ID={pk} was re-opened.")
+    return redirect("requests", pk=request.user.profile)
+
+@login_required(login_url='login')
+def delete_request(request, pk):
+    instance = WorkOrder.objects.get(id=pk)
+    instance.delete()
+
+    messages.success(request, f"Request with ID={pk} was deleted.")
+    return redirect("requests", pk=request.user.profile)
+
+@login_required(login_url='login')
+def completed_request(request, pk):
+    instance = WorkOrder.objects.get(id=pk)
+    instance.status = 'completed'
+    instance.reported_date =  timezone.now()
+    instance.save()
+
+    messages.success(request, f"Request with ID={pk} has been Completed.")
+    return redirect("requests", pk=request.user.profile)
