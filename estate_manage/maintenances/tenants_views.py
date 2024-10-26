08589c655ba8677ef2d7c06a408a0ef4b061398a
Chidera Divine-gift <chrischidera6@gmail.com>
2024-10-26 17:12:15 +0100

    "Updated templates, views, and URLs for tenant work order tracking and service provider details"

--- estate_manage/maintenances/tenants_views.py
+++ estate_manage/maintenances/tenants_views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .models import WorkOrder
 from .forms import WorkOrderForm
 
 
@@ -30,15 +31,33 @@ def request_submission(request, pk):
 
 @login_required(login_url='login')
 def tracking(request, pk):
+    status = request.GET.get('status', 'all')
+    workorder = WorkOrder.objects.filter(apartment__tenant_apartment__user=request.user.profile)
+    
+    open_request = workorder.filter(status='open')
+    in_progress_request = workorder.filter(status='in_progress')
+    completed_request = workorder.filter(status='completed')
+    closed_request = workorder.filter(status='closed')
+
     context = {
         'menu': 'workorder',
         's_menu': 'tau',
+        'open_count': open_request.count(),
+        'in_progress_count': in_progress_request.count(),
+        'completed_count': completed_request.count(),
+        'closed_count': closed_request.count(),
+        'open': open_request,
+        'in_progress': in_progress_request,
+        'completed': completed_request,
+        'closed': closed_request,
+        'workorder': workorder,
+        'status': status,
     }
 
     return render(request, 'tenant_work_order/tracking.html', context)
 
 @login_required(login_url='login')
-def service_provider(request, pk):
+def service_provider_details(request, pk):
     context = {
         'menu': 'workorder',
         's_menu': 'spi',
