1ff9e4f9f05c0a35b74276d1aef4e060faff7182
Chidera Divine-gift <chrischidera6@gmail.com>
2024-11-07 16:56:11 +0100

    Update views.py

--- estate_manage/notifications/views.py
+++ estate_manage/notifications/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Notification
 
-# Create your views here.
+def notifications(request, type, pk):
+    notification = Notification.objects.filter(user=request.user)
+    unread = notification.filter(status="UN")
+    read = notification.filter(status="RD")
+    context = {
+        'menu': 'notification',
+        'unread': unread,
+        'read': read,
+    }
+    return render(request, 'notify/notifications.html', context)
